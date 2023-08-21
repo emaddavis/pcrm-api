f8e0eaad3453e6c59082f06b4637129b99e80527
emaddavis <emaddavis@gmail.com>
2023-08-21 16:55:13 -0400

    API hello world

---
+++ src/test/java/co/personalcrm/pcrmapi/PcrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.personalcrm.pcrmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcrmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
